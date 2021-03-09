157463c58dfc04e9b3ac0fe93098235d92f5e2ad
pgirala <pgirala@gmail.com>
2021-03-09 09:48:13 +0100

    Captura el envío e invoca al servicio.

--- src/main/java/org/igae/modelo/Envio.java
+++ src/main/java/org/igae/modelo/Envio.java
@@ -1,7 +1,28 @@
 package org.igae.modelo;
 
+import java.util.Date;
+
 public class Envio {
+    private String idRemitente;
+    private String idDestinatario;
     private String comentario;
+    private Date momentoEnvio;
+
+    public String getIdRemitente() {
+        return idRemitente;
+    }
+
+    public void setIdRemitente(String idRemitente) {
+        this.idRemitente = idRemitente;
+    }
+
+    public String getIdDestinatario() {
+        return idDestinatario;
+    }
+
+    public void setIdDestinatario(String idDestinatario) {
+        this.idDestinatario = idDestinatario;
+    }
 
     public String getComentario() {
         return comentario;
@@ -11,10 +32,21 @@ public class Envio {
         this.comentario = comentario;
     }
 
+    public Date getMomentoEnvio() {
+        return momentoEnvio;
+    }
+
+    public void setMomentoEnvio(Date momentoEnvio) {
+        this.momentoEnvio = momentoEnvio;
+    }
+
     @Override
     public String toString() {
         return "Envio{" +
+                "idRemitente='" + idRemitente + '\'' +
+                "idDestinatario='" + idDestinatario + '\'' +
                 "comentario='" + comentario + '\'' +
+                "momentoEnvio=" + (momentoEnvio == null ? "null":momentoEnvio.toString()) + '\'' +
                 '}';
     }
 }
---
+++ src/main/java/org/igae/servicio/EnvioService.java
@@ -0,0 +1,90 @@
+package org.igae.servicio;
+
+
+import com.mongodb.*;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Filters;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.*;
+import com.mongodb.client.model.Sorts;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.exists;
+import static com.mongodb.client.model.Updates.set;
+
+import org.igae.modelo.Envio;
+
+@ApplicationScoped
+public class EnvioService {
+
+    @Inject
+    MongoDatabase mongoDB;
+
+    public void addEnvio(Envio envio) {
+        System.out.println("=========================<");
+        System.out.println(envio.toString());
+        System.out.println("=========================>");
+    }
+/*
+    public List<Usuario> list(){
+        List<Usuario> list = new ArrayList<>();
+        Bson filter = exists("data.password");
+        MongoCursor<Document> cursor = getCollection().find(filter).iterator();
+
+        try {
+            while (cursor.hasNext()) {
+                Document document = cursor.next();
+                Usuario usuario = new Usuario();
+                usuario.setEmail(document.get("data", Document.class).getString("email"));
+                System.out.println("===============================");
+                System.out.println(document.toString());
+                System.out.println("===============================");
+                list.add(usuario);
+            }
+        } finally {
+            cursor.close();
+        }
+        
+        return list;
+    }
+ 
+    publicvoid add(Usuario usuario){
+        Document document = new Document()
+                .append("name", customer.getName())
+                .append("surname", customer.getSurname())
+                .append("id", customer.getId());
+        getCollection().insertOne(document);
+    }
+
+    public void update(Customer customer){
+        // update one document
+
+        Bson filter = eq("id", customer.getId());
+        Bson updateOperation = set("name", customer.getName());
+        getCollection().updateOne(filter, updateOperation);
+    }
+
+    public void delete(Customer customer){
+        // delete one document
+
+        Bson filter = eq("id", customer.getId());
+        getCollection().deleteOne(filter);
+    }
+
+*/
+    private MongoCollection getCollection(){
+        return mongoDB.getCollection("submissions");
+    }
+}
